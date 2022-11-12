c81e8f9dc17dc24cd83a752a62e69990b8f630e3
zenrabbit <cj4nature@gmail.com>
2022-11-12 12:20:41 -0500

    table ok

---
+++ Prey_plotter/app0.R
@@ -0,0 +1,55 @@
+# Prey_species_tool
+# A simple tool to query reported prey species for federally listed vertebrates within Central California
+library(shiny)
+library(tidyverse)
+
+
+prey <- read_csv("tidy_prey.csv") %>% 
+  select(listed_species, prey_diet_binomial) %>% 
+  group_by(listed_species, prey_diet_binomial) %>% 
+  summarize(n = n()) %>% 
+  pivot_wider(names_from = listed_species, values_from = n) %>% 
+  mutate_at(c(2:17), ~replace_na(.,0))
+
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(    
+    
+    # Give the page a title
+    titlePanel("Prey item query tool for Central California listed vertebrates"),
+    
+    # Generate a row with a sidebar
+    sidebarLayout(      
+      
+      # Define the sidebar with one input
+      sidebarPanel(
+        selectInput("listed", "listed species:", 
+                    choices=colnames(prey[2:17])),
+        hr(),
+        helpText("Data from KNB")
+      ),
+      
+      # Create a spot for the barplot
+      mainPanel(
+        plotOutput("preyplot")  
+      )
+      
+    )
+  )
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  # Fill in the spot we created for a plot
+  output$preyplot <- renderPlot({
+    
+    # Render a barplot
+    barplot(prey[,input$listed], 
+            main=input$listed,
+            ylab="Number of Telephones",
+            xlab="Year")
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
